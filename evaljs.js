53f39cefc46301ebc7d02be7d41ed13cdbea22ab
Someone68 <68.someone.m@gmail.com>
2023-11-10 13:39:05 -0800

    focus pocus

--- evaljs.js
+++ evaljs.js
@@ -1,6 +1,8 @@
 /// script_execution.js
+let focusedclipboardwritetext = false;
 window.addEventListener("keyup", (event) => {
 	if (event.ctrlKey && event.code === "Backquote") {
+		focusedclipboardwritetext = false;
 		let command = prompt("Evaluate Command:");
 		try {
 			let copy = false;
@@ -11,11 +13,16 @@ window.addEventListener("keyup", (event) => {
 			let evaledCommand = eval(command);
 			alert(evaledCommand);
 			if (copy) {
-				document.documentElement.focus();
-				navigator.clipboard.writeText(evaledCommand);
+				document.addEventListener("focus", copyy);
 			}
 		} catch (err) {
 			alert(err);
 		}
 	}
 });
+
+function copyy() {
+	focusedclipboardwritetext = true;
+	navigator.clipboard.writeText(evaledCommand);
+	document.removeEventListener("focus", copyy);
+}
