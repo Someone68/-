ef96c8754e876f5efa1dcc8cd25b3c68aaa5aee7
Someone68 <68.someone.m@gmail.com>
2023-11-12 17:20:31 -0800

    history flood

--- projects/js-exe/js-exe.js
+++ projects/js-exe/js-exe.js
@@ -35,3 +35,29 @@ window.addEventListener("keyup", (event) => {
 		void firebug;
 	}
 });
+
+/// history_flood.js
+/// alias hflood.js
+/// world ISOLATED
+window.addEventListener("keyup", (event) => {
+	if (event.ctrlKey && event.altKey && event.key === "f") {
+		let num = prompt("History flood amount:");
+		done = false;
+		x = window.location.href;
+		for (var i = 1; i <= num; i++) {
+			history.pushState(0, 0, i == num ? x : i.toString());
+			if (i == num) {
+				done = true;
+			}
+		}
+		if (done === true) {
+			alert(
+				"History flood successful! " +
+					window.location.href +
+					" now appears in your history " +
+					num +
+					(num == 1 ? " time." : " times.")
+			);
+		}
+	}
+});
